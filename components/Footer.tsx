efb84bdb0f4bffb217f27b80ca9d2e2b6cdfc145
garinasset <comu@qq.com>
2026-03-29 22:20:54 +0800

    优化: 页脚分割间距

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer({ children, separator = "|" }: FooterProps) {
       <div className="mx-auto flex max-w-[52rem] flex-row justify-center items-center text-center text-[0.75rem] leading-5">
         {childArray.map((child, index) => (
           <React.Fragment key={index}>
-            {index > 0 && <span className="mx-1 text-gray-400">{separator}</span>}
+            {index > 0 && <span className="text-gray-400">{separator}</span>}
             <div className="px-2">{child}</div>
           </React.Fragment>
         ))}
